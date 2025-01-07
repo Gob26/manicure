626601abbb78bcd42f2dc87618943f186f17114b
gob26 <profservis26@gmail.com>
2025-01-07 23:01:11 +0300

    Добавлено логирование мастера

--- app/api/v1/masters/masters_read_router.py
+++ app/api/v1/masters/masters_read_router.py
@@ -3,7 +3,6 @@ from use_case.master_service.master_read_service import MasterReadService
 from db.schemas.master_schemas.master_schemas import MasterCreateSchema
 from config.components.logging_config import logger
 
-
 master_read_router = APIRouter()
 
 
@@ -17,5 +16,29 @@ async def get_master_by_city_and_slug_route(city_slug: str, master_slug: str):
     """
     Получение мастера по slug города и slug мастера.
     """
-    return await MasterReadService.get_master(city_slug, master_slug)
+    # Логируем начало запроса
+    logger.info(f"Запрос на получение мастера: Город - {city_slug}, Мастер - {master_slug}")
 
+    try:
+        # Пытаемся получить мастера через сервис
+        master = await MasterReadService.get_master(city_slug, master_slug)
+
+        # Если мастер не найден, выбрасываем ошибку
+        if not master:
+            logger.error(f"Мастер с slug {master_slug} в городе {city_slug} не найден.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Мастер с slug {master_slug} в городе {city_slug} не найден."
+            )
+
+        # Логируем успешный результат
+        logger.info(f"Мастер {master_slug} найден в городе {city_slug}.")
+        return master
+
+    except Exception as e:
+        # Логируем любые другие исключения
+        logger.error(f"Произошла ошибка при получении мастера: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Ошибка при получении данных о мастере."
+        )
