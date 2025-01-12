7de5fc7c6778cd8ba66e3308a05b9e7d5a2ccff2
gob26 <profservis26@gmail.com>
2025-01-12 14:59:40 +0300

    Категории ОК

--- app/db/repositories/services_repositories/category_service_repositories.py
+++ app/db/repositories/services_repositories/category_service_repositories.py
@@ -11,5 +11,86 @@ class ServiceCategoryRepository(BaseRepository):
 
     @classmethod
     async def create_category(cls, **kwargs: Any) -> Category:
-        logger.info(f"Мастер создан с данными: {kwargs}")
-        return await cls.create(**kwargs)
+        logger.info(f"Начало создания категории с параметрами: {kwargs}")
+        try:
+            category = await cls.create(**kwargs)
+            logger.info(f"Категория успешно создана: ID={category.id}")
+            return category
+        except Exception as e:
+            logger.error(f"Ошибка при создании категории: {e}")
+            raise
+
+    @classmethod
+    async def get_all_categories(cls) -> List[Category]:
+        """
+        Получение всех категорий из базы данных.
+        """
+        return await cls.get_all()
+
+    @classmethod
+    async def get_category_id(cls, category_id: int) -> Optional[Category]:
+        """
+        Получение категории по ее ID.
+        """
+        try:
+            # Сначала получаем корутину и ждем её выполнения
+            category = await cls.get_by_id(id=category_id)
+            # Затем делаем prefetch_related
+            await category.fetch_related('services')
+            return category
+        except DoesNotExist:
+            logger.error(f"Категория с ID {category_id} не найдена")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Категория с ID {category_id} не найдена"
+            )
+        except Exception as e:
+            logger.error(f"Ошибка при получении категории по ID {category_id}: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Внутренняя ошибка сервера"
+            )
+
+    @classmethod
+    async def update_category(cls, category_id: int, category_data: dict) -> Optional[Category]:
+        """
+        Обновление категории по ее ID.
+        """
+        try:
+            category = await cls.get_by_id(id=category_id)
+            if category:
+                updated_category = await cls.update(id=category_id, **category_data)
+                return updated_category
+            else:
+                return None
+            # raise HTTPException(status_code=404, detail="Категория не найдена")
+        except DoesNotExist:
+            logger.error(f"Категория с ID {category_id} не найдена")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Категория с ID {category_id} не найдена"
+            )
+        
+    @classmethod
+    async def delete_category(cls, category_id: int) -> None:
+        """
+        Удаление категории по ее ID.
+        """
+        # Пытаемся найти категорию
+        category = await cls.get_by_id(category_id)
+        if not category:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Категория с ID {category_id} не найдена"
+            )
+
+        # Удаляем категорию
+        deleted_count = await cls.delete(category_id)
+        if deleted_count == 0:
+            logger.error(f"Не удалось удалить категорию с ID {category_id}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Ошибка удаления категории с ID {category_id}"
+            )
+
+        logger.info(f"Категория с ID {category_id} успешно удалена")
--- app/use_case/service_service/category_service.py
+++ app/use_case/service_service/category_service.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from tortoise.exceptions import DoesNotExist
+
 from db.models.services_models.category_model import Category
 from db.repositories.services_repositories.category_service_repositories import ServiceCategoryRepository
 from use_case.utils.slug_generator import generate_unique_slug
@@ -10,7 +14,7 @@ class CategoryService:
         """
         Создание новой категории.
         """
-        slug = category_data.get("slug") or generate_unique_slug(Category, category_data["name"])
+        slug = category_data.get("slug") or await generate_unique_slug(Category, category_data["name"])
         category = await ServiceCategoryRepository.create_category(
             name=category_data["name"],
             slug=slug,
@@ -22,4 +26,52 @@ class CategoryService:
         services = await category.services.all()  # Получаем все связанные услуги
 
         return category, services
+    
+    @staticmethod
+    async def get_all_categories() -> list[Category]:
+        """
+        Получение всех категорий через репозиторий.
+        """
+        categories = await ServiceCategoryRepository.get_all_categories()
+        return await Category.all().prefetch_related('services')
 
+    @staticmethod
+    async def get_category_by_id(category_id: int) -> Optional[Category]:
+        try:
+            category = await ServiceCategoryRepository.get_category_id(category_id=category_id)
+            return category
+        except DoesNotExist:
+            return None
+        
+    @staticmethod
+    async def update_category(category_id: int, category_data: dict) -> Optional[Category]:
+        """
+        Обновление категории.
+        """
+        try:
+            category = await ServiceCategoryRepository.get_category_id(category_id=category_id)
+            if category:
+                updated_category = await ServiceCategoryRepository.update_category(
+                    category_id=category_id,
+                    category_data=category_data
+                )
+                return updated_category
+            else:
+                return None
+        except DoesNotExist:
+            return None    
+
+    @staticmethod
+    async def delete_category(category_id: int) -> bool:
+        """
+        Удаление категории.
+        """
+        try:
+            category = await ServiceCategoryRepository.get_category_id(category_id=category_id)
+            if category:
+                await ServiceCategoryRepository.delete_category(category_id=category_id)
+                return True
+            else:
+                return False
+        except DoesNotExist:
+            return False
